5c62083f8dd31ae23447c754e10d33360096c857
Benjamin Reiter <ben@wake.io>
2018-11-20 20:17:23 -0500

    moved to its own repo

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+import simplejson as json
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wake@localhost/docent'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    api_id = db.Column(db.Integer, nullable=False)
+    repository = db.Column(db.String(1028), nullable=False)  # no shit make this its own object TODO
+    highlight = db.Column(db.Boolean, nullable=False)
+    public_domain = db.Column(db.Boolean, nullable=False)
+    primary_image = db.Column(db.String(1028), nullable=False)
+    #  TODO handle "additionalImages"
+    title = db.Column(db.String(1028), nullable=False)
+    department = db.Column(db.String(1028), nullable=False)  # make this an id / reference?
+    artist = db.Column(db.String(1028), nullable=False)  # definitely make an artist table
+    artist_bio = db.Column(db.String(1028))  # duhhhhhhhh
+    obj_date = db.Column(db.String(1028))  # make this into a DATE! Maybe a great thing to test a tensor flow parser on! #machinelearning #$$$$
+    obj_begin_date = db.Column(db.String(1028))
+    obj_end_date = db.Column(db.String(1028))
+    medium = db.Column(db.String(1028), nullable=False)
+    dimensions = db.Column(db.String(1028), nullable=False)
+
+    def __repr__(self):
+        return '<Item {},{}>'.format(self.title, self.id)
+
+    def tiny(self):
+        return {'id': self.id, 'title': self.title, 'primary_image': self.primary_image}
+
+    def full(self):
+        d = {}
+        for column in self.__table__.columns:
+            d[column.name] = str(getattr(self, column.name))
+        return d
+
+# db.create_all()
+
+
+@app.route("/")
+def index():
+    return json.dumps({"items": [item.tiny() for item in Item.query.all()]})
+
+
+@app.route("/<int:item_id>")
+def get_item(item_id):
+    return json.dumps(Item.query.filter(Item.id==item_id).one().full())
+
+
+
---
+++ loader.py
@@ -0,0 +1,74 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import requests
+from app import Item
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wake@localhost/docent'
+db = SQLAlchemy(app)
+
+
+def loader():
+    top_id_obj = Item.query.order_by(Item.id.desc()).first()
+    print top_id_obj
+    base_url = "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+    obj_ids = requests.get(base_url).json()['objectIDs']
+    index = obj_ids.index(top_id_obj.id)
+    obj_ids = obj_ids[index + 1:]
+    print len(obj_ids)
+    counter = 0
+    save_total = 0
+    errors = []
+    for obj_id in obj_ids:
+        counter += 1
+        response = requests.get(base_url + "/{}".format(obj_id)).json()
+        new_item_dict = {
+            "api_id": response.get("objectID"),
+            "repository": response.get("repository"),
+            "highlight": response.get("isHighlight", False),
+            "public_domain": response.get("isPublicDomain", True),
+            "primary_image": response.get("primaryImage"),
+            "title": response.get("title"),
+            "department": response.get("department"),
+            "artist": response.get("artistDisplayName"),
+            "artist_bio": response.get("artistDisplayBio"),
+            "obj_date": response.get("objectDate"),
+            "obj_begin_date": response.get("objectBeginDate"),
+            "obj_end_date": response.get("objectEndDate"),
+            "medium": response.get("medium"),
+            "dimensions": response.get("dimensions")
+        }
+        if new_item_dict['primary_image'].startswith("http"):
+            new_item = Item(**new_item_dict)
+            try:
+                print "{}, {}".format(new_item.title, new_item.artist)
+                db.session.add(new_item)
+                db.session.commit()
+            except:
+                errors.append(new_item_dict)
+                db.session.rollback()
+            if counter >= 100:
+                save_total += counter
+                print "saving, {} remaining".format(len(obj_ids) - save_total)
+                db.session.commit()
+                counter = 0
+    db.session.commit()
+    print "done"
+    print errors
+
+loader()
+
+#   'city': 'Pittsburgh',
+#   'state': 'Pennsylvania',
+#   'county': '',
+#   'country': 'United States',
+#   'region': 'Mid-Atlantic',
+#   'subregion': '',
+#   'locale': '',
+#   'locus': '',
+#   'excavation': '',
+#   'river': '',
+#   'classification': 'Glass',
+#   'rightsAndReproduction': '',
+#   'linkResource': '',
+
