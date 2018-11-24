02cdd2bbe887f9ab2f8c108957810afb7620029d
Benjamin Reiter <ben@wake.io>
2018-11-23 19:58:08 -0500

    using rijksmuseum for highlights

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import simplejson as json
 
 app = Flask(__name__)
 CORS(app)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wake@localhost/docent'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def index():
-    return json.dumps({"items": [item.tiny() for item in Item.query.all()]})
+    return json.dumps({"items": [item.tiny() for item in Item.query.filter_by(repository_id=2).all()]})
 
 
 @app.route("/<int:item_id>")
