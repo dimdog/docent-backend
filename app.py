ba34c3d31b149c77959de8e20a8e8d0e0c1ce4ba
Benjamin Reiter <ben@wake.io>
2018-11-25 15:40:54 -0500

    more testing login objects

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
@@ -43,17 +43,19 @@ def login():
     id_info = id_token.verify_oauth2_token(as_json.get("tokenId"), google_request, audience='633799705698-fs81n284e1iv4318fk2vdclksv29d82e.apps.googleusercontent.com')
     print(id_info)
     print("-----")
-    print(as_json)
     if "iss" in id_info and id_info["iss"] == "accounts.google.com" \
             and as_json["email"] == id_info["email"]:
-        print("hi")
         user = User.query.filter_by(email=as_json["email"]).first()
         if not user:
             user = User(family_name=id_info["family_name"], given_name=id_info["given_name"], full_name=id_info["name"],
                         image_url=id_info["picture"], email=as_json["email"], locale=id_info["locale"])
             db.session.add(user)
             db.session.commit()
+        else:
+            print("Found user:{}").format(user.to_json())
         login_user(user)
+        print(session)
+        session['user_id'] = user.id
     return json.dumps(user.to_json())
 # {'iss': 'accounts.google.com', 'azp': '633799705698-fs81n284e1iv4318fk2vdclksv29d82e.apps.googleusercontent.com', 'aud': '633799705698-fs81n284e1iv4318fk2vdclksv29d82e.apps.googleusercontent.com', 'sub': '101477866356937362560', 'email': 'dimdog@gmail.com', 'email_verified': True, 'at_hash': 'FrCen2m7wpa-WwTxfWta0g', 'name': 'Ben Reiter', 'picture': 'https://lh6.googleusercontent.com/-37vcW9X8k70/AAAAAAAAAAI/AAAAAAAADy8/oqM2ebjWLxQ/s96-c/photo.jpg', 'given_name': 'Ben', 'family_name': 'Reiter', 'locale': 'en', 'iat': 1543177052, 'exp': 1543180652, 'jti': 'db71a5490749bf79be98b9f685d7dc223159c6d4'}
 
