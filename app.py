4a45fbb8a549eefe3eefd551fcf84afa8b3b4e4b
Benjamin Reiter <ben@wake.io>
2018-11-25 14:11:31 -0500

    more testing

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 from model import Item
+from google.oauth2 import id_token
+from google.auth.transport import requests
 from google.auth import jwt
 
 import simplejson as json
 
+google_request = requests.Request()
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
@@ -28,8 +31,11 @@ def login():
     print(dir(request))
     as_json = request.get_json()
     print(as_json)
-    claims = jwt.decode(as_json.get("accessToken"), verify=False)
+    id_info = id_token.verify_oauth2_token(as_json.get("tokenId"), google_request, 'docentapp.com')
+    claims = jwt.decode(as_json.get("tokenId"), verify=False)
     print("-----")
+    print(id_info)
+    print("%%%%%%%%%%")
     print(claims)
     print("-----")
     return json.dumps(request.args)
