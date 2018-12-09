ab3f3c9f7ea4709f9ddc4a8dfd8cc05231a9f109
Benjamin Reiter <ben@wake.io>
2018-12-09 11:31:35 -0500

    attempt to get images being read, but can't get the images to load from the urls

--- mig.py
+++ mig.py
@@ -1,17 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+import imageio
+import urllib.request
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 db = SQLAlchemy(app)
-from model import Item, ItemLanguage
+from model import Item
+
+fname="temp.{}"
+
+for item in db.session.query(Item)[:1]:
+    if item.primary_image and not (item.primary_image_height or item.primary_image_width):
+        print(item.primary_image)
+        file_name = fname.format(item.primary_image.split(".")[-1])
+        urllib.request.urlretrieve(item.primary_image, file_name)
+        im = imageio.imread(file_name)
+        print(dir(im))
+        print(im)
+        print(im.shape)
+        print(im.meta)
 
 
-for item in db.session.query(Item):
-    il = ItemLanguage(item_id=item.id, language="EN", title=item.title, medium=item.medium, dimensions=item.dimensions,
-                      creditLine=item.creditLine)
-    db.session.add(il)
-
-db.session.commit()
+# db.session.commit()
