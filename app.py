61143a96df9827d03ed302ab5b99dcbe5145e467
Benjamin Reiter <ben@wake.io>
2018-12-17 10:44:38 -0500

    need some refactoring now that i know what we want related more

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def item_response(db_user, item_id):
     item = Item.query.filter(Item.id == item_id).one()   # do better than 500 on error
     response = item.full()
     response['artist_other_works'] = [i.tiny() for i in Item.query.filter(not Item.id == item_id).filter_by(artist_id=item.artist_id)]
-    response['medium_other_works'] = [i.tiny() for i in Item.query.filter(not Item.id == item_id).filter_by(medium=item.medium)]
+    response['department_other_works'] = [i.tiny() for i in Item.query.filter(not Item.id == item_id).filter_by(department_id=item.department_id)]
     if not db_user.is_anonymous:
         response["liked"] = item_id in db_user.likes
         response["user"] = db_user.to_json()
